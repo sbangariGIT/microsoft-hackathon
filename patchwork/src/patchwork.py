6326b460dd000a36609d1e2caecafc9c8418c2c4
sbangariGIT <bangarisairohan@gmail.com>
2024-10-19 15:09:46 -0700

    //

--- patchwork/src/patchwork.py
+++ patchwork/src/patchwork.py
@@ -3,12 +3,12 @@ from openai import OpenAI
 import os
 import sys
 from dotenv import load_dotenv
+import tiktoken
 
 # Load environment variables from the .env file
 load_dotenv()
-
 client = OpenAI()
-
+MODEL = "gpt-4o"
 # Function to encode the image
 def encode_image(image_path):
   with open(image_path, "rb") as image_file:
@@ -18,7 +18,7 @@ def encode_image(image_path):
 # Getting the base64 string
 def analyze(content):
     response = client.chat.completions.create(
-    model="gpt-4o",
+    model=MODEL,
     messages=[
         {
         "role": "user",
@@ -37,22 +37,27 @@ def process_directory(directory_path):
             "type": "text",
             "text": "Based on all these images, can you tell how this screen time is influencing the user",
             }]
+    tokens = 0
+    encoder = tiktoken.encoding_for_model(MODEL)
     # Load all image files from the directory
     for filename in os.listdir(directory_path):
         if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif', '.tiff')):
             image_path = os.path.join(directory_path, filename)
             base64_image = encode_image(image_path)
+            num_tokens = encoder.encode(base64_image)
+            tokens += len(num_tokens)
             texts.append({
             "type": "image_url",
             "image_url": {
                 "url":  f"data:image/jpeg;base64,{base64_image}"
             }
             })
+    print(f"Number of tokens used {tokens}")
     if len(texts) > 1:
         print("Analyzing extracted text for mental health impact...")
-        analysis = analyze(texts)
+        # analysis = analyze(texts)
         print("\n--- Mental Health Impact Analysis ---")
-        print(analysis)
+        # print(analysis)
     else:
         print("No images from this file.")
 
